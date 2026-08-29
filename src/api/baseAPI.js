c74917fa5de125618805b8e49aa5d0d3657c4711
samiulfahad <samiul_fahad@yahoo.com>
2026-08-30 00:44:41 +0600

    updated

--- src/api/baseAPI.js
+++ src/api/baseAPI.js
@@ -4,7 +4,7 @@ const cloud = "https://client-api.labpilotpro.com/v1"
 const local = "http://127.0.0.1:3000/v1"
 
 const baseAPI = axios.create({
-  baseURL: local, // change to your Fastify server URL
+  baseURL: cloud, // change to your Fastify server URL
   headers: {
     "Content-Type": "application/json",
   },
